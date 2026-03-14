eb7d97b5edaa15ec1d9791f732ca5ee2a75255e9
Femto-0 <arits.king74@gmail.com>
2026-03-13 19:55:08 -0500

    no longer used....

--- src/MatrixSplitter.java
+++ src/MatrixSplitter.java
@@ -12,7 +12,7 @@ public class MatrixSplitter {
            int subRow = currentRow + splitSize;
 
            int[][] newMatrix = new int[subRow][originalMatrix[0].length];
-
+           System.out.println("Matrix split by rows: ");
            for (int i = currentRow; i <= subRow - 1; i++) {
                for (int j = 0; j < originalMatrix[0].length; j++) {
                    newMatrix[i][j] = originalMatrix[i][j];
@@ -35,7 +35,7 @@ public class MatrixSplitter {
            int subColumn = currentColumn + splitSize;
 
            int[][] newMatrix = new int[originalMatrix.length][subColumn];
-
+           System.out.println("Matrix split by columns: ");
            for (int i = 0; i < originalMatrix.length; i++) {
                for (int j = currentColumn; j <= subColumn-1; j++) {
                    newMatrix[i][j] = originalMatrix[i][j];
@@ -48,8 +48,8 @@ public class MatrixSplitter {
     }
 
     public static void main(String[] args) {
-        int[][] test= {{1,2},{3,4}, {5, 6}};
-        int splitSize=3;
+        int[][] test= {{1,2,  3, 4},{3,4,5, 6}, {5, 6, 7, 8}};
+        int splitSize=2;
         MatrixSplitter matrixSplitter= new MatrixSplitter();
         int currentRow=test.length;
         for(int i=0; i<=currentRow-1;) {
