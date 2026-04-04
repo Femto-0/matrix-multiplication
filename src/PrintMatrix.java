f86b9fc7d8e673e002610687d3f1f0d77b271f18
Femto-0 <arits.king74@gmail.com>
2026-04-03 23:30:09 -0500

    added constructor to help identify what matrix is getting printed easily

--- src/PrintMatrix.java
+++ src/PrintMatrix.java
@@ -1,6 +1,20 @@
 public class PrintMatrix {
 
-    public void printMatrix(int[][] matrix){
+    int[][] matrix;
+    String nameOfMatrix;
+
+    public PrintMatrix(int[][] matrix, String nameOfMatrix){
+        this.matrix=matrix;
+        this.nameOfMatrix=nameOfMatrix;
+    }
+
+    public PrintMatrix(int [][] matrix){
+        this.matrix=matrix;
+    }
+    public void printMatrix(){
+        if(!nameOfMatrix.isEmpty()){
+            System.out.println(nameOfMatrix+":");
+        }
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 System.out.print(matrix[i][j]+ " ");
